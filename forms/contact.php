2fe1ac6cb1173be65b3b9739f6a832998a8f29df
Wahyu1011 <wahyuekosetyopribowo@gmail.com>
2024-08-28 23:58:30 +0700

    Add files via upload

--- forms/contact.php
+++ forms/contact.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  $receiving_email_address = 'wahyuekosetyopribowo@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -24,14 +24,14 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+  
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
+    'host' => 'smtp.elasticemail.com',
+    'username' => 'maswahyu@gmail.com',
+    'password' => '960018FAC12B440C15801CA48982F02718F2',
+    'port' => '2525'
   );
-  */
+  
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
