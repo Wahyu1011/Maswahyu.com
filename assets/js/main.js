357df5de8fafbc869522d14d2326acb95e8a3c76
Wahyu1011 <wahyuekosetyopribowo@gmail.com>
2025-01-31 22:52:16 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -281,21 +281,21 @@ updateClock(); // Initial call to display clock immediately
 
 /**Wajah Lucu */
 
-document.querySelector(eye)
-.addEventListener('mousemove', eyeBall);
-function eyeBall() {
+document.querySelectorAll('.eye').forEach(function(eye) {
+    eye.addEventListener('mousemove', eyeBall);
+});
+
+function eyeBall(event) {
     let eye = document.querySelectorAll('.eye');
     eye.forEach(function(eye) {
         let x = (eye.getBoundingClientRect().left) + (eye.clientWidth / 2);
         let y = (eye.getBoundingClientRect().top) + (eye.clientHeight / 2);
 
         let radian = Math.atan2(event.pageX - x, event.pageY - y);
-        let rot = (radian * (180 / Math.PI) * - 1) + 270;
+        let rot = (radian * (180 / Math.PI) * -1) + 270;
         eye.style.transform = "rotate(" + rot + "deg)";
-    })
-};
-
-
+    });
+}
 
 function startCountdown(targetDate) {
     let timerDisplay = document.getElementById("timer");
