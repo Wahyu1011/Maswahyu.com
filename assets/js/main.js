f20895c38e34d2a1834b61b9226b000b85aab71e
Wahyu1011 <wahyuekosetyopribowo@gmail.com>
2025-01-31 23:14:16 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -298,29 +298,4 @@ function eyeBall(event) {
 }
 
 
-    function updateCountdown() {
-      const targetDate = new Date("March 1, 2025 00:00:00").getTime();
-      const now = new Date().getTime();
-      const timeRemaining = targetDate - now;
-
-      const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
-      const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
-      const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
-
-      document.getElementById("days").textContent = days;
-      document.getElementById("hours").textContent = hours;
-      document.getElementById("minutes").textContent = minutes;
-      document.getElementById("seconds").textContent = seconds;
-
-      if (timeRemaining < 0) {
-        document.querySelector(".countdown-container").innerHTML = "<div class='title'>Selamat Menunaikan Ibadah Puasa!</div>";
-      }
-    }
-
-    setInterval(updateCountdown, 1000);
-    updateCountdown(); // Call once to initialize
-
-}
-
-
+   
